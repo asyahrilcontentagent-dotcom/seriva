578dc99ec6acaa150d001cef433d83dc6fe157df
asyahrilcontentagent-dotcom <asyahril.contentagent@gmail.com>
2026-04-14 07:08:59 +0800

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,33 @@
+"""Configuration package for SERIVA."""
+
+from .constants import (  # noqa: F401
+    RoleInfo,
+    ROLE_ID_NOVA,
+    ROLE_ID_IPAR_TASHA,
+    ROLE_ID_TEMAN_KANTOR_IPEH,
+    ROLE_ID_TEMAN_LAMA_WIDYA,
+    ROLE_ID_WANITA_BERSUAMI_SISKA,
+    ROLE_ID_TERAPIS_AGHIA,
+    ROLE_ID_TERAPIS_MUNIRA,
+    ROLE_ID_TEMAN_SPESIAL_DAVINA,
+    ROLE_ID_TEMAN_SPESIAL_SALLSA,
+    ROLES,
+    list_role_summaries,
+    get_role_info,
+)
+
+__all__ = [
+    "RoleInfo",
+    "ROLE_ID_NOVA",
+    "ROLE_ID_IPAR_TASHA",
+    "ROLE_ID_TEMAN_KANTOR_IPEH",
+    "ROLE_ID_TEMAN_LAMA_WIDYA",
+    "ROLE_ID_WANITA_BERSUAMI_SISKA",
+    "ROLE_ID_TERAPIS_AGHIA",
+    "ROLE_ID_TERAPIS_MUNIRA",
+    "ROLE_ID_TEMAN_SPESIAL_DAVINA",
+    "ROLE_ID_TEMAN_SPESIAL_SALLSA",
+    "ROLES",
+    "list_role_summaries",
+    "get_role_info",
+]
